deda0eacd1ca9d9112c353f3a0ce9a019c6c240d
gracefrisbee <58480265+gracefrisbee@users.noreply.github.com>
2026-07-06 19:46:23 -0700

    Update guest.js

--- guest.js
+++ guest.js
@@ -1,19 +1,8 @@
 window.guests = {
     "Lydia": {
         title: "Miss Lydia",
-        society: "Hartfield Society",
-        announcement: "announcements/lydia.mp3"
-    },
-
-    "Hailey": {
-        title: "Miss Hailey",
-        society: "Pemberley Society",
-        announcement: "announcements/hailey.mp3"
-    },
-
-    "Chelsea": {
-        title: "Miss Chelsea",
-        society: "Highbury Society",
-        announcement: "announcements/chelsea.mp3"
+        society: "Hartfield Society"
     }
 };
+
+alert("Guest database loaded!");
