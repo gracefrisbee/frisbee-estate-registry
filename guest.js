473827883998fddb12bc0bd1319d89ba5bcd7e4a
gracefrisbee <58480265+gracefrisbee@users.noreply.github.com>
2026-07-06 18:59:59 -0700

    Update guest.js

--- guest.js
+++ guest.js
@@ -1,1 +1,19 @@
+const guests = {
+    "Lydia": {
+        title: "Miss Lydia",
+        society: "Hartfield Society",
+        announcement: "announcements/lydia.mp3"
+    },
 
+    "Hailey": {
+        title: "Miss Hailey",
+        society: "Pemberley Society",
+        announcement: "announcements/hailey.mp3"
+    },
+
+    "Chelsea": {
+        title: "Miss Chelsea",
+        society: "Highbury Society",
+        announcement: "announcements/chelsea.mp3"
+    }
+};
