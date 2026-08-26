52919ea9f3fea55775e8f287e4777096bf225d44
gracefrisbee <58480265+gracefrisbee@users.noreply.github.com>
2026-08-26 12:16:47 -0700

    Update guest.js

--- guest.js
+++ guest.js
@@ -1,7 +1,165 @@
 window.guests = {
+
+    "Addison Boomsma": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/addisonboomsma.mp3"
+    },
+
+    "Alli": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/alli.mp3"
+    },
+
+    "April Jean": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/apriljean.mp3"
+    },
+
+    "Audrey Houl": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/audreyhoul.mp3"
+    },
+
+    "Caroline Bjerre": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/carolinebjerre.mp3"
+    },
+
+    "Chelsea Gorecki": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/chelseagorecki.mp3"
+    },
+
+    "Carla Gorecki": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/carlagorecki.mp3"
+    },
+
+    "Corinne Liu": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/corinneliu.mp3"
+    },
+
+    "Duchess Caroline Frisbee": {
+        title: "Duchess",
+        society: "The Frisbee Estate",
+        audio: "audio/duchesscarolinefrisbee.mp3"
+    },
+
+    "Ella Iervolino": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/ellaiervolino.mp3"
+    },
+
+    "Esther Kim": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/estherkim.mp3"
+    },
+
+    "Faythe Piper": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/faythepiper.mp3"
+    },
+
+    "Gabby": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/gabby.mp3"
+    },
+
+    "Hailey Hurtado": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/haileyhurtado.mp3"
+    },
+
+    "Jennifer Hurtado": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/jenniferhurtado.mp3"
+    },
+
+    "Hannah Lindeman": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/hannahlindeman.mp3"
+    },
+
+    "Julia Mangum": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/juliamangum.mp3"
+    },
+
+    "Karli Lonnquist": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/karlilonnquist.mp3"
+    },
+
+    "Katie Buck": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/katiebuck.mp3"
+    },
+
+    "Kim Prowten": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/kimprowten.mp3"
+    },
+
+    "Laci Dueck": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/lacidueck.mp3"
+    },
+
+    "Lydia Chia": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/lydiachia.mp3"
+    },
+
+    "Michaela Lindeman": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/michaelalindeman.mp3"
+    },
+
+    "Nicole Richie": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/nicolerichie.mp3"
+    },
+
+    "Sarah Buck": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/sarahbuck.mp3"
+    },
+
+    "Taylor Esparza": {
+        title: "Miss",
+        society: "The Esteemed Company",
+        audio: "audio/tayloresparza.mp3"
+    },
+
     "Grace Frisbee": {
         title: "Lady",
         society: "Hostess of The Frisbee Estate",
         audio: "audio/gracefrisbee.mp3"
     }
+
 };
