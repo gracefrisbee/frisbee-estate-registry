c2fa2d2e4dde5dbc30db035efbda1b9154d84348
gracefrisbee <58480265+gracefrisbee@users.noreply.github.com>
2026-08-26 12:08:39 -0700

    Update guest.js

--- guest.js
+++ guest.js
@@ -1,8 +1,30 @@
 window.guests = {
-    "Lydia": {
+
+    "Grace Frisbee": {
+        title: "Lady",
+        society: "Hostess of The Frisbee Estate",
+        audio: "audio/gracefrisbee.mp3"
+    },
+
+
+    "Jane Austen": {
         title: "Miss",
-        society: "Hartfield Society"
+        society: "The Literary Society",
+        audio: "audio/janeausten.mp3"
+    },
+
+
+    "Elizabeth Bennet": {
+        title: "Miss",
+        society: "The Bennet Family",
+        audio: "audio/elizabethbennet.mp3"
+    },
+
+
+    "Charlotte Brontë": {
+        title: "Miss",
+        society: "The Brontë Family",
+        audio: "audio/charlottebronte.mp3"
     }
 
 };
-
